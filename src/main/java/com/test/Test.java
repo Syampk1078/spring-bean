8a86665a67bf458743a9744cdcffb7c628eec8c4
KANKANALA SYAM PRASAD <119304851+Syampk1078@users.noreply.github.com>
2024-12-26 12:49:59 +0530

    Both different references print same hashcode

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -1,12 +1,19 @@
 package com.test;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 
 	public static void main(String[] args) {
 		//By default Spring IOC will be created objects
-		new ClassPathXmlApplicationContext("Beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		Car obj = context.getBean("car",Car.class);
+		System.out.println(obj.hashCode());
+		
+		Car obj1 = context.getBean("car",Car.class);
+		System.out.println(obj1.hashCode());
 		
 	}
 
